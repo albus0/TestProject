2a13c309eecb7ff1ffe268c1f4359f680a475325
Albina Lutfullina <106741510+albinalutfullina@users.noreply.github.com>
2023-02-12 16:11:09 +0500

    Create TabBarController.

---
+++ TestProject/TabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarController.swift
+//  TestProject
+//
+//  Created by Альбина on 12.02.2023.
+//
+
+import UIKit
+import SnapKit
+
+class TabBarController: UITableViewController, UITabBarControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
