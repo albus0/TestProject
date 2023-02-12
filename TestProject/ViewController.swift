8f6dcae416cbc3afeb9866a7c6c1daca15410195
Albina Lutfullina <106741510+albinalutfullina@users.noreply.github.com>
2023-02-12 15:00:41 +0500

    Add button.

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
+    
+    // MARK: -Outlets
+    
+    private lazy var button: UIButton = {
+        let button = UIButton(type: .roundedRect)
+        button.backgroundColor = .systemRed
+        button.setTitle("Press here", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.layer.cornerRadius = 25.0
+        button.layer.shadowColor = UIColor.blue.cgColor
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowOffset = .zero
+        button.layer.shadowRadius = 10
+        button.layer.shouldRasterize = true
+        button.layer.rasterizationScale = UIScreen.main.scale
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+        setupHerarchy()
+        setupLayout()
     }
+    
+    //MARK: - setup
+    
+    private func setupHerarchy() {
+        view.addSubview(button)
+    }
+    
+    private func setupLayout() {
+        button.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.centerY.equalTo(view).offset(300)
+            make.height.equalTo(30)
+            make.right.equalTo(view).offset(-50)
+            make.left.equalTo(view).offset(50)
+            
+        }
+    }
+    
+    
 
 
 }
